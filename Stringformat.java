cefcc74fc577984a2950ab9fc5e4785e7580273e
Raayavegas <veeralarithika@gmail.com>
2024-12-02 16:05:33 +0530

    Add files via upload

---
+++ Stringformat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=String.format("congratulations %s you won 3 lacks",a);
+        System.out.println(b);
+    }
+}
