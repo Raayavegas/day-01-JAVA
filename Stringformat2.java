d285cc113c6f9a76b8967698d4927b6dea869223
Raayavegas <veeralarithika@gmail.com>
2024-12-02 16:15:37 +0530

    Add files via upload

---
+++ Stringformat2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class c 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int c=sc.nextInt();
+        String b=String.format("congratulations %s and %d you won 5 lacks",a,c);
+        System.out.println(b);
+    }
+}
