cbe95f081e142b5bd05893d305a54f26d01f9d39
Raayavegas <veeralarithika@gmail.com>
2024-12-02 14:40:47 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class A
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        char b='A';
+        float c=3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
