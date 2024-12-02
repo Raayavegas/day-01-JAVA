994f5d5ed8e6c340bbe78cd52c99e5e154d0a768
Raayavegas <veeralarithika@gmail.com>
2024-12-02 15:31:05 +0530

    Add files via upload

---
+++ printingnxtchar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+        char c='A';
+        int B=c;
+        B+=1;
+        char v=(char)(B);
+        System.out.println(v);
+    }
+}
